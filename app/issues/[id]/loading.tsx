c3364d82ddfcd88c682a65005acbe23927dffec9
Lounis <lounischerifi29@gmail.com>
2024-11-04 13:24:35 +0100

    adding  others skeleton loading

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,10 +1,20 @@
+import { Box, Flex, Card } from '@radix-ui/themes'
 import React from 'react'
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const NewIssuesLoading = () => {
   return (
-    <div>
-      Loading...
-    </div>
+    <Box className='max-w-xl'>
+    <Skeleton/>
+    <Flex gap={"3"} my={"2"}>
+    <Skeleton width="5rem"/>
+    <Skeleton width="8rem"/>    
+    </Flex>
+    <Card className='prose' mt={"3"}>
+    <Skeleton count={3}/>
+    </Card>
+    </Box>
   )
 }
 
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -3,12 +3,10 @@ import { Table } from "@radix-ui/themes";
 import React from "react";
 import IssuesStatusBadge from "../components/IssuesStatusBadge";
 import IssueActions from "./IssueActions";
-import delay from "delay";
 import Link from "../components/Link";
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany();
-  await delay(2000);
   return (
     <div>
       <IssueActions />

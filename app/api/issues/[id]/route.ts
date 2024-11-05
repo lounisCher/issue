ab7e50515b51ca39ed0b44c4c119acbcc0a02572
Lounis <lounischerifi29@gmail.com>
2024-11-05 15:30:22 +0100

    build api endpoint for deleting issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -26,3 +26,17 @@ export async function PATCH(request:NextRequest, {params}: {params: Promise<{ id
     return NextResponse.json(updatedIssue);
 
 }
+
+export async function DELETE(request:NextRequest, {params}:{params:  Promise<{ id: string }>}){
+    const idIssue = (await params).id;
+    const id = parseInt(idIssue, 10);   
+    const toDeleteIssue = await prisma.issue.findUnique({
+        where:{id: id}
+    });
+    if(!toDeleteIssue) return NextResponse.json({error: 'Invalid Issue'}, {status:404});
+    await prisma.issue.delete({
+        where:{id: id}
+    });
+    return NextResponse.json({});
+
+}

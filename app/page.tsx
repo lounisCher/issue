30b520c419866ba6f216e0e19838d3dd4166efc2
Lounis <lounischerifi29@gmail.com>
2024-11-06 22:21:59 +0100

    adding latest issue and Issue Summary

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
-import Pagination from "./_components/Pagination";
+import prisma from "@/prisma/client";
+import IssueSummary from "./IssueSummary";
 
-export default function Home() {
+
+
+export default async function Home() {
+ const open = await prisma.issue.count({where:{status:"OPEN"}});
+ const inProgress = await prisma.issue.count({where:{status:"IN_PROGRESS"}});
+ const closed = await prisma.issue.count({where:{status:"CLOSED"}});
+
   return (
-    <div>
-      
-    </div>
+   <IssueSummary open={open} inProgress={inProgress} closed={closed}/>
   );
 }

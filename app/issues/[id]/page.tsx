32c29932789b6a78e979656f6dab6b2be4fdc036
Lounis <lounischerifi29@gmail.com>
2024-11-08 18:41:19 +0100

    adding metadata and optimize perf with react cache

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -6,40 +6,57 @@ import IssueDetails from "./IssueDetails";
 import DeleteIssueButton from "./DeleteIssueButton";
 import { auth } from "@/app/auth";
 import AssigneeSelect from "./AssigneeSelect";
+import { cache } from "react";
 
 
+const fetchUser = cache((issueId: number) =>
+  prisma.issue.findUnique({ where: { id: issueId } })
+);
 
-const IssueDetailPage = async ({params}: {params: Promise<{ id: string }>}) => {
-
+const IssueDetailPage = async ({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) => {
   const session = await auth();
-  
-
 
   const idIssue = (await params).id;
   if (!idIssue) notFound();
   const id = parseInt(idIssue, 10);
   if (isNaN(id)) return notFound();
-  const issue = await prisma.issue.findUnique({
-    where: { id: id },
-  });
-  if(!issue) return notFound();
+
+  const issue = await fetchUser(id);
+  if (!issue) return notFound();
   return (
-    <Grid columns={{initial: "1", sm:"5"}}>
+    <Grid columns={{ initial: "1", sm: "5" }}>
       <Box className="md:col-span-4">
-        <IssueDetails issue={issue}/>
+        <IssueDetails issue={issue} />
       </Box>
-      {session && 
-      <Box className="mt-2 lg:m-0">
-        <AssigneeSelect issue={issue} /> 
-        <Flex direction="column"> 
-        <EditIssuesButton issueId={issue.id}/>
-        <DeleteIssueButton issueId={issue.id}/>
-        </Flex>
-      </Box>}
+      {session && (
+        <Box className="mt-2 lg:m-0">
+          <AssigneeSelect issue={issue} />
+          <Flex direction="column">
+            <EditIssuesButton issueId={issue.id} />
+            <DeleteIssueButton issueId={issue.id} />
+          </Flex>
+        </Box>
+      )}
     </Grid>
   );
 };
 
-
+export async function generateMetada({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const idIssue = (await params).id;
+  const id = parseInt(idIssue, 10);
+  const issue = await fetchUser(id);
+  return {
+    title: issue?.title,
+    Description: "Details of issue" + issue?.id,
+  };
+}
 
 export default IssueDetailPage;
